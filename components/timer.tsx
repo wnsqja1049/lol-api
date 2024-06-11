12c81e3eab23951e3ab8eb779ebd753cf8cb89e0
wnsqja1049 <wnsqja1049@gmail.com>
2024-06-11 21:30:27 +0900

    오류수정

--- components/timer.tsx
+++ components/timer.tsx
@@ -47,27 +47,31 @@ export const CompactGameDuration = ({gameDuration} : {gameDuration: number}) => 
 	);
 };
 
-export const TimeStampToTimeBefore = ({timeStamp} : {timeStamp: number}) => {
-    var end = Math.floor(new Date(timeStamp).getTime() / 1000);
-    let now = Math.floor(Date.now() / 1000);
-
-	let value = now - end;
+export const TimeStampToTimeBefore = ({timeStamp} : {timeStamp: number | undefined}) => {
+	if(timeStamp) {
+		var end = Math.floor(new Date(timeStamp).getTime() / 1000);
+		let now = Math.floor(Date.now() / 1000);
 	
-	let beforeDays = Math.floor(value / (24 * 60 * 60));
-	let beforeHours = Math.floor(value / (60 * 60));
-	let beforeMinutes = Math.floor(value / 60);
-
-	if(beforeDays >= 1) {
-		return (
-			<div>{beforeDays}일 전</div>
-		);
-	} else if(beforeHours >= 1) {
-		return (
-			<div>{beforeHours}시간 전</div>
-		);
-	} else if(beforeMinutes) {
-		return (
-			<div>{beforeMinutes}분 전</div>
-		);
+		let value = now - end;
+		
+		let beforeDays = Math.floor(value / (24 * 60 * 60));
+		let beforeHours = Math.floor(value / (60 * 60));
+		let beforeMinutes = Math.floor(value / 60);
+	
+		if(beforeDays >= 1) {
+			return (
+				<div>{beforeDays}일 전</div>
+			);
+		} else if(beforeHours >= 1) {
+			return (
+				<div>{beforeHours}시간 전</div>
+			);
+		} else if(beforeMinutes) {
+			return (
+				<div>{beforeMinutes}분 전</div>
+			);
+		}
+	} else {
+		return <></>
 	}
 };
