dd78627d9dba501464cd8efd569a046a8d725466
Junbeom Kim <52428949+wnsqja1049@users.noreply.github.com>
2024-07-09 14:52:19 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,25 @@
 /** @type {import('next').NextConfig} */
 
+const { headers } = require('next/headers');
+
 const API_KEY = process.env.API_KEY;
 
 const nextConfig = {
     reactStrictMode: false,
+    async headers() {
+        return [
+            {
+              // matching all API routes
+              source: "/api/:path*",
+              headers: [
+                { key: "Access-Control-Allow-Credentials", value: "true" },
+                { key: "Access-Control-Allow-Origin", value: "*" },
+                { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+                { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+              ]
+            }
+          ]
+    },
     async rewrites() {
         return [
             {
